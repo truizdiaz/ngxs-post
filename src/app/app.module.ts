e5ec0fed60e173b6f136989ddda6c9aa5c3ef9e6
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-08-09 09:02:19 -0300

    nuevo post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { ListarPostComponent } from './components/listar-post/listar-post.component';
 import { NuevoPostComponent } from './components/nuevo-post/nuevo-post.component';
+
+// Modulos
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +19,10 @@ import { NuevoPostComponent } from './components/nuevo-post/nuevo-post.component
     NuevoPostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/nuevo-post/nuevo-post.component.ts
+++ src/app/components/nuevo-post/nuevo-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interfaces/post';
+import { v4 as uuidv4 } from 'uuid';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nuevo-post',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo-post.component.css']
 })
 export class NuevoPostComponent implements OnInit {
+  nombre: string = '';
+  descripcion: string = '';
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  agregarPost() {
+
+    // Validamos campos
+    if (this.nombre == '' || this.descripcion == '') {
+      this.toastr.error('El campo texto y descriopcion son obligatorios', 'Error');
+      return;
+    }
+
+    // Creamos objeto
+    const post: Post = {
+      id: uuidv4(),
+      nombre: this.nombre,
+      descripcion: this.descripcion
+    }
+
+    // Mostramos mensaje de exito
+    this.toastr.success(`El post ${this.nombre.toUpperCase()} fue agregado con exito!`, 'Post registrado!');
+
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.nombre = '';
+    this.descripcion = '';
   }
 
 }
