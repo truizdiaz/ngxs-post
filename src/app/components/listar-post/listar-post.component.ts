d1d50e708adca8a656b7275d7d4257f0e3089830
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-08-09 09:18:17 -0300

    listar post html

--- src/app/components/listar-post/listar-post.component.ts
+++ src/app/components/listar-post/listar-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interfaces/post';
 
 @Component({
   selector: 'app-listar-post',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-post.component.css']
 })
 export class ListarPostComponent implements OnInit {
+  listPost: Post[] = [ 
+    {
+      id: 'd24242-dadad',
+      nombre: 'Bootstrap',
+      descripcion: "Es un framework css con un poderoso sistema de regillas"
+    },
+    {
+      id: 'dadada-dadad',
+      nombre: 'Angular',
+      descripcion: "Es un framework para crear SPA creado por Google"
+    },
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  eliminarPost(id: string) {
+    console.log(id);
+    // this.listPost = this.listPost.filter(post => post.id !== id);
   }
 
 }
